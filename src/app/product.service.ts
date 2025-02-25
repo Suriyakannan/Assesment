d7c8cb068d2a919ef2974098746e62a17b7d3491
Suriyakannan <120772778+Suriyakannan@users.noreply.github.com>
2025-02-25 22:01:36 +0530

    intial comit

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,18 @@
+// src/app/services/product.service.ts
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // This makes the service available throughout the app
 })
 export class ProductService {
+  private apiUrl = 'http://localhost:3000/products';  //Your NestJS API endpoint
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
 }
